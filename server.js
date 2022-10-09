3242c3cc809ad1e66670609adb634db6226d5cc1
matthew-m-morton <mor21018@byui.edu>
2022-10-08 21:37:47 -0600

    -week 4- md5 addition

--- server.js
+++ server.js
@@ -21,26 +21,41 @@ app.listen(port, async ()=>{
 app.get('/',(req,res)=>{
     res.send('Hello World!')
 });
+
 app.post('/user', (req,res)=>{
     const newUserRequestObject = req.body;
+    const loginPassword = req.body.password;
+    const hash = md5(loginPassword);
+    console.log(hash);
+    newUserRequestObject.password=hash;
+    newUserRequestObject.verifyPassword = hash
     console.log('New User:',JSON.stringify(newUserRequestObject));
     redisClient.hSet('users',req.body.email,JSON.stringify(newUserRequestObject));
+
     res.send('New User'+newUserRequestObject.email+' added');
 
 })
 
-app.post("/login", (req,res)=>{
+app.post("/login", async (req,res)=>{
     const loginEmail = req.body.userName;
     console.log(JSON.stringify(req.body));
     console.log("loginEmail", loginEmail);
-    const loginPassword = req.body.password;
+    const loginPassword = md5(req.body.password);
     console.log("loginPassword", loginPassword);
 
-    if (loginEmail == "name42@gmail" && loginPassword == "P@ssw0rd"){
-        const token = uuidv4();
-        res.send(token);
-    } else{
-        res.status(401);//unauthorized
-        res.send("Invalid user or password");
+    const userString=await redisClient.hGet('users',loginEmail);
+    const userObject=JSON.parse(userString)
+    if(userString=='' || userString==null){
+        res.status(404);
+        res.send('User not found');
     }
+
+
+    else if (loginEmail == userObject.userName && loginPassword == userObject.password){
+            const token = uuidv4();
+            res.send(token);
+    } else{
+            res.status(401);//unauthorized
+            res.send("Invalid user or password");
+        }
 })
